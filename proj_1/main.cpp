3adcdb7f67f6ef74cb00d1f03ed32e6c043b71b3
HiIAmTzeKean <ngtzekean@gmail.com>
2025-02-19 14:39:19 +0800

    chore: commit main file to test

--- proj_1/main.cpp
+++ proj_1/main.cpp
@@ -1,5 +1,5 @@
 #include "storage/storage.h"
-
+#include "bp_tree.h"
 int main() {
     std::string inputFile = "games.txt";
     Storage storage;
@@ -10,19 +10,24 @@ int main() {
         return 1;
     }
 
+    BPlusTree tree = BPlusTree(3);
+    for (Record &record : records) {
+        tree.insert(record.fg_pct_home, &record);
+        tree.print();
+    }
 
-    std::string outputFile = "nba_database.dat";
-    storage.writeDatabaseFile(outputFile, records);
+    // std::string outputFile = "nba_database.dat";
+    // storage.writeDatabaseFile(outputFile, records);
 
-    std::vector<Record> loadedRecords;
-    storage.readDatabaseFile(outputFile, loadedRecords);
+    // std::vector<Record> loadedRecords;
+    // storage.readDatabaseFile(outputFile, loadedRecords);
 
-    // Task 1: Storage component
-    storage.reportStatistics(loadedRecords);
+    // // Task 1: Storage component
+    // storage.reportStatistics(loadedRecords);
 
-    // Task 3: Search for rows with attribute “FG_PCT_home” from 0.6 to 0.9 (both inclusively)
-    // Brute-force linear scan
-    storage.bruteForceScan(records, 0.6, 0.9);
+    // // Task 3: Search for rows with attribute “FG_PCT_home” from 0.6 to 0.9 (both inclusively)
+    // // Brute-force linear scan
+    // storage.bruteForceScan(records, 0.6, 0.9);
 
     return 0;
 }
