9e8a47aa07c296f9cde1757c0c140cab4876dc26
HiIAmTzeKean <ngtzekean@gmail.com>
2025-04-02 17:14:52 +0800

    Add logger

---
+++ proj_2/project.py
@@ -0,0 +1,13 @@
+import logging
+
+
+# Configure the global logger
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger('DBPiper')
